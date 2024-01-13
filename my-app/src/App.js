6ac59a4d778c18af6e7188b84a484e1cb22518f9
Shrikant <141563150+Shrikant204@users.noreply.github.com>
2024-01-13 15:13:49 +0000

    js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,16 @@
-// App.js
 import React, { useState } from 'react';
 
-const App = () => {
-  const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+function App() {
+
   const [pass1, setPass1] = useState('');
   const [pass2, setPass2] = useState('');
 
-  function generate ()  {
+  function generatePassword ()  {
+
     let generatedPass1 = '';
     let generatedPass2 = '';
+
+    const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
     for (let i = 1; i <= 15; i++) {
       generatedPass1 += characters[Math.floor(Math.random() * characters.length)];
@@ -23,7 +24,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Your React App</h1>
-      <button onClick={generate}>Generate Passwords</button>
+      <button onClick={generatePassword}>Generate Passwords</button>
       <div>
         <input type="text" value={pass1} disabled />
         <input type="text" value={pass2} disabled />
