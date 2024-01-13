6c1e3c540dd55a5c9cb98d11d987f7f631a9416e
Shrikant <141563150+Shrikant204@users.noreply.github.com>
2024-01-13 16:21:32 +0000

    configuration changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,7 +12,7 @@ function App() {
 
     const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-    for (let i = 1; i <= 15; i++) {
+    for (let i = 1; i <= 20; i++) {
       generatedPass1 += characters[Math.floor(Math.random() * characters.length)];
       generatedPass2 += characters[Math.floor(Math.random() * characters.length)];
     }
